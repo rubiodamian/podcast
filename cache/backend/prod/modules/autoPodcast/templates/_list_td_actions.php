e20f33e2864d8745a27db7f14b6403df1e67db22
root <root@alex-notebook.(none)>
2013-04-01 15:24:34 -0300

    a

--- cache/backend/prod/modules/autoPodcast/templates/_list_td_actions.php
+++
@@ -1,9 +0,0 @@
-<td>
-  <ul class="sf_admin_td_actions">
-    <?php echo $helper->linkToEdit($podcast, array(  'params' =>   array(  ),  'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
-    <?php if ($sf_user->hasCredential('God')): ?>
-<?php echo $helper->linkToDelete($podcast, array(  'credentials' => 'God',  'params' =>   array(  ),  'confirm' => 'Are you sure?',  'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
-<?php endif; ?>
-
-  </ul>
-</td>
--- cache/backend/prod/modules/autoPodcast/templates/_list_th_stacked.php
+++
@@ -1,1 +0,0 @@
-<?php include_partial('podcast/list_th_tabular', array('sort' => $sort)) ?>
